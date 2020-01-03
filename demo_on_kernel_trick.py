30d4bcc6a34d1ad091aa207143679cc9d11763f8
I.N <48551497+leztien@users.noreply.github.com>
2020-01-03 22:47:47 +0100

    Update demo_on_kernel_trick.py

--- demo_on_kernel_trick.py
+++ demo_on_kernel_trick.py
@@ -23,25 +23,40 @@ X = np.random.randn(10,2)
 degree = 5
 Xpoly = poly(X, degree=degree)
 
-
+#
 from itertools import product
 g = product(X,X)
 t = tuple(g)
-b = 0
-dot_products1 = [(np.dot(t[0],t[1])+b)**degree for t in t]
+dot_products1 = [(np.dot(t[0],t[1])+1)**degree for t in t]
 dot_products1 = minmax(dot_products1)
+nx = np.argsort(dot_products1)
+nx = nx[5:-5]
+dot_products1 = dot_products1[nx]
 
+#
 g = product(Xpoly, Xpoly)
 t = tuple(g)
 dot_products2 = [np.dot(t[0],t[1]) for t in t]
 dot_products2 = minmax(dot_products2)
-r = dot_products1.mean() - dot_products2.mean()
-dot_products2 = dot_products2 + r
+dot_products2 = dot_products2[nx]
 
-plt.plot(dot_products1, marker='.', alpha=0.5)
-plt.plot(dot_products2, marker='.', alpha=0.5)
+#
+from sklearn.preprocessing import PolynomialFeatures
+Xpoly = PolynomialFeatures(degree=degree, include_bias=True, interaction_only=False).fit_transform(X)
+g = product(Xpoly, Xpoly)
+t = tuple(g)
+dot_products3 = [np.dot(t[0],t[1]) for t in t]
+dot_products3 = minmax(dot_products3)
+dot_products3 = dot_products3[nx]
 
-t = np.sort(tuple(np.quantile(dot_products1, q=[0.25,0.75])) + tuple(np.quantile(dot_products2, q=[0.25,0.75])))[[0,-1]]
+
+plt.plot(dot_products1, marker='.', alpha=0.5, color='r')
+plt.plot(dot_products2, marker='.', alpha=0.5, color='g')
+plt.plot(dot_products3, marker='.', alpha=0.5, color='b')
+
+t = np.sort(tuple(np.quantile(dot_products1, q=[0.25,0.75])) + 
+            tuple(np.quantile(dot_products2, q=[0.25,0.75])) + 
+            tuple(np.quantile(dot_products3, q=[0.25,0.75])))[[0,-1]]
 plt.ylim(t[0]-0.015, t[1]+0.015)
 
 
